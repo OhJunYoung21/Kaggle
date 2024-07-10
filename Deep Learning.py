14c941c88c9abdf807bf7b2276d1017d9b64c1e7
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-07-10 11:14:09 +0900

    init commit

--- Deep Learning.py
+++ Deep Learning.py
@@ -1,7 +1,3 @@
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Create a network with 1 linear unit
-model = keras.Sequential([
-    layers.Dense(units=1, input_shape=[3])
-])
